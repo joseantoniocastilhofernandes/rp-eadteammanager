cff71dfc8e309bf6cb7f914aba86c8c402536a4e
Jose Castilho <jose.antonio.castilho@gmail.com>
2024-03-20 19:18:34 -0300

    primeira versao

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ module.exports = {
     esmExternals: false,
     jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
   },
+   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
