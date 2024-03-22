c54eb8d4145b1b4106f1ca1c888d3e32e92bc9cc
Jose Castilho <jose.antonio.castilho@gmail.com>
2024-03-21 22:00:50 -0300

    primeira versao

--- src/pages/relatorio-de-estudo/index.js
+++ src/pages/relatorio-de-estudo/index.js
@@ -212,7 +212,7 @@ console.log('componentDidMount');
       var usuarioLogado = JSON.parse(user);
       axios({
           method: 'get',
-          url: SERVICES_CONTEXT+ '/api/empreendedorservice/carregarResumoRelatorioDeEstudos?idEmpreendedor='+idEmpreendedor  +'&idCurso=2&idPatrocinador='+ usuarioLogado.idEmpreendedor ,
+          url: SERVICES_CONTEXT+ '/empreendedorservice/carregarResumoRelatorioDeEstudos?idEmpreendedor='+idEmpreendedor  +'&idCurso=2&idPatrocinador='+ usuarioLogado.idEmpreendedor ,
           withCredentials: false,
         }
       ).then(function (response) {
